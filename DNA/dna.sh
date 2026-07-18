4b4ab0701e3dd3f7a57c7a25a27d5b37c8081857
Rish <266709254+RishOnBash@users.noreply.github.com>
2026-07-18 08:16:46 +0530

    update dna.sh

--- DNA/dna.sh
+++ DNA/dna.sh
@@ -3,9 +3,8 @@
 # count the nucleotide frequency from a file
 
 INPUT=$1
-
 [[ ! -f $INPUT ]] && \
-    { echo "Usage: ${0##/*} <file>"; exit 1; }
+    echo "Usage: ${0##/*} <file>" && exit 1
 
 nt_arr=(A C G T)
 for nt in "${nt_arr[@]}"; do
