515631fe303153c0c607094cae90ac792612c273
Rish <266709254+RishOnBash@users.noreply.github.com>
2026-07-19 19:31:56 +0530

    update rna.py

--- RNA/rna.py
+++ RNA/rna.py
@@ -7,11 +7,7 @@ if (len(sys.argv) < 2):
     print(f"Usage: {argv[0]} <file>")
     exit(1)
 
-thy='T'
-ura='U'
-
 with open(argv[1], "r") as file:
     data = file.read()
 
-data = data.replace(thy, ura)
-print(data)
+print(data.replace('T', 'U'))
