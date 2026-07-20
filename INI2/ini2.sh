88377cb3a5d1a10f02ed645980b3c80bf33bfae6
Rish <266709254+RishOnBash@users.noreply.github.com>
2026-07-20 14:41:59 +0530

    refactor ini2.sh

--- INI2/ini2.sh
+++ INI2/ini2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-(($# < 2)) && \
-    echo "Usage: ${0##/*} <num1> <num2>" && exit 1
+FILE=$1
+[[ ! -f $FILE ]] &&
+    echo "Usage: $0 <file>" && exit 1
 
-num1=$(($1 ** 2))
-num2=$(($2 ** 2))
+read num1 num2 <$FILE
 
-echo "$((num1 + num2))"
+echo "$((num1 ** 2 + num2 ** 2))"
